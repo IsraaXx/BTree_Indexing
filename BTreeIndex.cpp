61cf39c62425c32bf6ea668255da8cf099b22127
NadaBadawyy <148383483+NadaBadawyy@users.noreply.github.com>
2025-01-25 20:35:37 +0200

    add search functions

--- BTreeIndex.cpp
+++ BTreeIndex.cpp
@@ -518,6 +518,91 @@ int BTreeIndex::UpdateAfterInsert(int parentRecordNumber, int newChildRecordNumb
     }
     return newFromSplitIndex;
 }
+bool BTreeIndex::isEmpty(int recordNumber)
+{
+    return read_val(recordNumber, 0) == -1;
+}
+
+bool BTreeIndex::isLeaf(int recordNumber)
+{
+    return read_val(recordNumber, 0) == 0;
+}
+
+bool BTreeIndex::record_valid(int recordNumber) const
+{
+    if (recordNumber <= 0 || recordNumber > numberOfRecords)
+        return false;
+
+    return true;
+}
+
+int BTreeIndex::read_val(int rowIndex, int columnIndex)
+{
+    BTreeFile.seekg(0, std::ios::beg);
+    int x;
+    for (int i = 0; i < rowIndex * (2 * m + 1) + columnIndex + 1; i++)
+    {
+        BTreeFile >> x;
+    }
+
+    return x;
+}
+
+vector<pair<int, int>> BTreeIndex::read_node_values(int recordNumber)
+{
+    if (record_valid(recordNumber))
+    {
+
+        vector<pair<int, int>> theNode;
+
+        for (int i = 1; i <= numberOfRecords; i += 2)
+        {
+            int key = read_val(recordNumber, i);
+            int value = read_val(recordNumber, i + 1);
+            theNode.emplace_back(key, value);
+        }
+        return theNode;
+    }
+    else
+    {
+        return {};
+    }
+}
+
+int BTreeIndex::SearchARecord(const char *filename, int RecordID)
+{
+    if (isEmpty(1))
+        return -1;
+    std::vector<std::pair<int, int>> current;
+
+    int i = 1;
+    bool found;
+    while (!isLeaf(i))
+    {
+        current = read_node_values(i);
+        found = false;
+        for (auto p : current)
+        {
+            if (p.first >= RecordID)
+            {
+                i = p.second;
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+            return -1;
+    }
+
+    current = read_node_values(i);
+
+    for (auto pair : current)
+        if (pair.first == RecordID)
+            return pair.second;
+
+    return -1;
+}
 
 
 
